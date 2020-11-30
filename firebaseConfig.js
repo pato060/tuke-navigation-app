57f06ccb70f09b6e98432d8b338c260cc9452249
Patrik Urda <patrik.urda@student.tuke.sk>
2020-11-30 23:22:16 +0100

    Update

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD63knh5XrMpAOH-4QGTdxVVBtR6Hxaz5k",
+    authDomain: "tuke-navigation-app.firebaseapp.com",
+    databaseURL: "https://tuke-navigation-app.firebaseio.com",
+    projectId: "tuke-navigation-app",
+    storageBucket: "tuke-navigation-app.appspot.com",
+    messagingSenderId: "487780849447",
+    appId: "1:487780849447:web:05f9c53e42fcb570df3b02",
+    measurementId: "G-60FN88DXTV"
+  }
